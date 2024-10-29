93fd6bd62ef69781836a30944b982d71a4075768
unknown <ahsanul.ratan@gmail.com>
2024-10-29 23:37:48 +0600

    class28

---
+++ script.js
@@ -0,0 +1,55 @@
+
+
+// let marks;
+
+// marks = -3;
+
+// if(marks > 100 || marks <0){
+//     console.log('Invalid marks')
+// }
+
+// else if(marks >= 80){
+//     console.log( 'The grade is A+');
+// }
+    
+
+// else if(marks >= 70){
+//     console.log( 'The grade is A');
+// }
+    
+
+
+// else if(marks >=60){
+//     console.log("The grade is: A-")
+// }
+
+// else if(marks >=50){
+//     console.log('The grade is: B')
+// }
+
+// else if(marks >= 40){
+//     console.log('The grade is: C')
+// }
+
+// else if(marks >=33){
+//     console.log('The grade is:D')
+// }
+
+
+// else{
+//     console.log('The grade is: F')
+// }
+
+let number;
+number = 31;
+
+if(number % 2 == 0)
+    console.log("This is an even number");
+
+else{
+    console.log("This is an odd number");
+}
+
+
+
+
